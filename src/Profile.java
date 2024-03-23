6ab94cbef6aba2e4ad25bdcaf8bbd32888269956
zyte09 <allen.aranez32@gmail.com>
2024-03-23 16:08:25 +0800

    first commit

---
+++ src/Profile.java
@@ -0,0 +1,71 @@
+public class Profile {
+    private String name, gender, activity, goal;
+    private int age, weight, height;
+
+    public Profile(String name, String gender, String activity, String goal, int age, int weight, int height) {
+        this.name = name;
+        this.gender = gender;
+        this.activity = activity;
+        this.goal = goal;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    //get and set name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
