29b67dba9305017c459dbf98c4259da1f224d53a
zyte09 <allen.aranez32@gmail.com>
2024-03-29 20:55:27 +0800

    added meal

---
+++ src/Meal.java
@@ -0,0 +1,11 @@
+public class Meal {
+    String mealName;
+    String foodName;
+    double servingSize;
+
+    public Meal(String mealName, String foodName, double servingSize) {
+        this.mealName = mealName;
+        this.foodName = foodName;
+        this.servingSize = servingSize;
+    }
+}
